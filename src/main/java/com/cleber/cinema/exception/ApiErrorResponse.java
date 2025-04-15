c2f8133410f7e97a79705a8c6343558a47a363eb
Cleberbrant <cleberobb@hotmail.com>
2025-04-15 01:10:53 -0300

    reactor: refatorando ApiErrorResponse

--- src/main/java/com/cleber/cinema/exception/ApiErrorResponse.java
+++ src/main/java/com/cleber/cinema/exception/ApiErrorResponse.java
@@ -2,10 +2,12 @@ package com.cleber.cinema.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ApiErrorResponse {
 	private int statusCode;
 	private String message;
