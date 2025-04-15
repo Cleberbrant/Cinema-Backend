86932693429d3cb8600fc735b1881ddb0214b27e
Cleberbrant <cleberobb@hotmail.com>
2025-04-14 23:59:35 -0300

    config: adicionando config flyway e swagger

---
+++ src/main/java/com/cleber/cinema/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.cleber.cinema.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Cinema API")
+						.version("1.0")
+						.description("API para gestão de sistema de cinema")
+						.contact(new Contact()
+								.name("Suporte")
+								.email("suporte@cinema.com"))
+						.license(new License()
+								.name("Apache 2.0")
+								.url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+	}
+}
