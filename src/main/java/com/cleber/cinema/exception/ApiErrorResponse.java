1e39d5d9a5b5117f4da937a298510bb5667b7335
Cleberbrant <cleberobb@hotmail.com>
2025-05-06 10:09:07 -0300

    feat: adicionando lombok na ApiErrorResponse

--- src/main/java/com/cleber/cinema/exception/ApiErrorResponse.java
+++ src/main/java/com/cleber/cinema/exception/ApiErrorResponse.java
@@ -1,42 +1,15 @@
 package com.cleber.cinema.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApiErrorResponse {
 	private int statusCode;
 	private String message;
 	private LocalDateTime timestamp;
-
-	public ApiErrorResponse() {
-	}
-
-	public ApiErrorResponse(int statusCode, String message, LocalDateTime timestamp) {
-		this.statusCode = statusCode;
-		this.message = message;
-		this.timestamp = timestamp;
-	}
-
-	public int getStatusCode() {
-		return statusCode;
-	}
-
-	public void setStatusCode(int statusCode) {
-		this.statusCode = statusCode;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public LocalDateTime getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(LocalDateTime timestamp) {
-		this.timestamp = timestamp;
-	}
 }
