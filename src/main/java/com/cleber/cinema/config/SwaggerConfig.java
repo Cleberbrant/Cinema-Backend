574c299cce8ce33967d2ac50ebae0e51f5461bcc
Cleberbrant <cleberobb@hotmail.com>
2025-05-06 00:44:58 -0300

    feat: alterando configuradao do swagger para conter autenticacao bearer

--- src/main/java/com/cleber/cinema/config/SwaggerConfig.java
+++ src/main/java/com/cleber/cinema/config/SwaggerConfig.java
@@ -4,11 +4,16 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+	private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
 	@Bean
 	public OpenAPI customOpenAPI() {
@@ -22,6 +27,14 @@ public class SwaggerConfig {
 								.email("suporte@cinema.com"))
 						.license(new License()
 								.name("Apache 2.0")
-								.url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+								.url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+				.addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+				.components(new Components()
+						.addSecuritySchemes(SECURITY_SCHEME_NAME,
+								new SecurityScheme()
+										.name(SECURITY_SCHEME_NAME)
+										.type(SecurityScheme.Type.HTTP)
+										.scheme("bearer")
+										.bearerFormat("JWT")));
 	}
 }
